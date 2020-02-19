5cff0111a70076a1a9151bbb39702ad296ff54ca
rodrigo1990 <mcd77.1990@gmail.com>
2020-02-19 16:24:07 -0300

    Initial commit

---
+++ js/slider.js
@@ -0,0 +1,18 @@
+ $('#owl-1').owlCarousel({
+    loop:true,
+    margin:0,
+    autoplay:true,
+    autoplayTimeout:4000,
+    nav:false,
+    dots:true,
+    items:1,
+    responsive:{
+        0:{
+            items:1
+        },
+        1200:{
+            items:1
+        }
+    }
+});
+
